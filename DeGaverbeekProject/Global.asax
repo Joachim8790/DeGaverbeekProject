cb13637d072826d81ce8b874bd3a7cdb75a104f1
joach <joach@DESKTOP-M5K8GTK>
2017-10-06 19:04:10 +0200

    Add project files.

---
+++ DeGaverbeekProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DeGaverbeekProject.MvcApplication" Language="C#" %>
---
+++ DeGaverbeekProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DeGaverbeekProject.Startup))]
+namespace DeGaverbeekProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
