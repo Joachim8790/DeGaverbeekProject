0c02c180281c487baa635e4291b8de9d01ce1031
joach <joach@DESKTOP-M5K8GTK>
2017-11-10 18:10:24 +0100

    section folder en contact

---
+++ DeGaverbeekProject/ViewModels/IndexViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DeGaverbeekProject.ViewModels
+{
+    public class IndexViewModel
+    {
+        [Display(Name ="Naam en voornaam")]
+        public string name { get; set; }
+        [Display(Name = "Emailadres")]
+        public string emailaddress { get; set; }
+        [Display(Name = "Onderwerp")]
+        public string subject { get; set; }
+        [Display(Name = "Bericht")]
+        public string message { get; set; }
+    }
+}
