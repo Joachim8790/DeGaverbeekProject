dac3ea393d4bfae7884e5365528e80118a24c7dc
joach <joach@DESKTOP-M5K8GTK>
2018-04-17 19:49:37 +0200

    stage 3

--- DeGaverbeekProject/Models/AccountViewModels.cs
+++ DeGaverbeekProject/Models/AccountViewModels.cs
@@ -64,21 +64,19 @@ namespace DeGaverbeekProject.Models
 
     public class RegisterViewModel
     {
-        [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
+        public string CompanyName { get; set; }
+        public string CompanyActivity { get; set; }
+        public string FirstNameContact { get; set; }
+        public string LastNameContact { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public string County { get; set; }
+        public string VATNumber { get; set; }
         public string Email { get; set; }
-
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "Password")]
-        public string Password { get; set; }
-
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
+        public string Telephone { get; set; }
+        public string Fax { get; set; }
     }
 
     public class ResetPasswordViewModel
